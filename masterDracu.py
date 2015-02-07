c920ce67d73972de7929ac8b65e0834b4f04ab3b
Dina Marcaida Fajardo <fajardod91@yahoo.com.ph>
2015-02-07 11:59:17 +0800

    edited button

--- masterDracu.py
+++ masterDracu.py
@@ -13,8 +13,78 @@ import levels
 
 from itertools import chain
 from dracu import Dracu
+pygame.init()
+pygame.font.init()
+ 
+# Set the height and width of the screen
+size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
+screen = pygame.display.set_mode(size)
+    
+# load images to use
+icon = pygame.image.load("includes/img/icon.png")
+logo = pygame.image.load("includes/img/dracu-logo.png")
+btn = pygame.image.load("includes/img/btn.png")
+p = pygame.image.load("includes/img/play.png")
+pH = pygame.image.load("includes/img/playH.png")
+pA = pygame.image.load("includes/img/playA.png")
+pAH = pygame.image.load("includes/img/playAH.png")
+m = pygame.image.load("includes/img/back-to-menu.png")
+mH = pygame.image.load("includes/img/back-to-menuH.png")
 
-        
+# set the initial image of the buttons
+btn_play = p
+btn_menu = m 
+ 
+pygame.display.set_caption("DRACU-ESCAPE")
+pygame.display.set_icon(icon)
+
+# play music if ON
+conn = sqlite3.connect("dracuDb.s3db")
+cur = conn.execute("SELECT * FROM dracuOption")
+first_row = next(cur)
+for row in chain((first_row,),cur):
+    if str(row[0]) == "ON": 
+        pygame.mixer.music.play(10)
+    
+# Create the player
+dracu = Dracu()
+ 
+# Create all the levels
+level_list = []
+level_list.append(levels.Level_01(dracu))
+level_list.append(levels.Level_02(dracu))
+level_list.append(levels.Level_03(dracu))
+ 
+# Set the current level
+current_level_no = 0
+current_level = level_list[current_level_no]
+ 
+active_sprite_list = pygame.sprite.Group()
+dracu.level = current_level
+ 
+dracu.rect.x = 340
+dracu.rect.y = 400
+active_sprite_list.add(dracu)
+ 
+# Loop until the user clicks the close button.
+play = False
+play_again = False
+over = False
+    
+# font color of PLAY to be interactive
+play_color = constants.GREEN
+quit_color = constants.RED
+
+# initialize sounds/music
+burningSound = pygame.mixer.Sound('includes/sound/burning.wav')
+pygame.mixer.music.load('includes/music/dracu-level1.wav')
+    
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+notif= "GAME OVER"
+score = 0
+    
 def display_text(color, text, font_size, x, y, w, h):
     pygame.font.init()
     
@@ -23,79 +93,12 @@ def display_text(color, text, font_size, x, y, w, h):
     textRect = textSurface.get_rect()
     textRect.center = ((x+(w/2)), (y+(h/2)))
     return textSurface, textRect
+
     
 def main():
     """ Main Program """
-    pygame.init()
-    pygame.font.init()
- 
-    # Set the height and width of the screen
-    size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
-    screen = pygame.display.set_mode(size)
-
-    # load images to use
-    icon = pygame.image.load("includes/img/icon.png")
-    logo = pygame.image.load("includes/img/dracu-logo.png")
-    p = pygame.image.load("includes/img/play.png")
-    pH = pygame.image.load("includes/img/playH.png")
-    pA = pygame.image.load("includes/img/playA.png")
-    pAH = pygame.image.load("includes/img/playAH.png")
-    m = pygame.image.load("includes/img/back-to-menu.png")
-    mH = pygame.image.load("includes/img/back-to-menuH.png")
-
-    # set the initial image of the buttons
-    btn_play = p
-    btn_menu = m 
- 
-    pygame.display.set_caption("DRACU-ESCAPE")
-    pygame.display.set_icon(icon)
-
-    # play music if ON
-    conn = sqlite3.connect("dracuDb.s3db")
-    cur = conn.execute("SELECT * FROM dracuOption")
-    first_row = next(cur)
-    for row in chain((first_row,),cur):
-        if str(row[0]) == "ON": 
-            pygame.mixer.music.play(10)
     
-    # Create the player
-    dracu = Dracu()
- 
-    # Create all the levels
-    level_list = []
-    level_list.append(levels.Level_01(dracu))
-    level_list.append(levels.Level_02(dracu))
-    level_list.append(levels.Level_03(dracu))
- 
-    # Set the current level
-    current_level_no = 0
-    current_level = level_list[current_level_no]
- 
-    active_sprite_list = pygame.sprite.Group()
-    dracu.level = current_level
- 
-    dracu.rect.x = 340
-    dracu.rect.y = 400
-    active_sprite_list.add(dracu)
- 
-    # Loop until the user clicks the close button.
-    play = False
-    play_again = False
-    over = False
     
-    # font color of PLAY to be interactive
-    play_color = constants.GREEN
-    quit_color = constants.RED
-
-    # initialize sounds/music
-    burningSound = pygame.mixer.Sound('includes/sound/burning.wav')
-    pygame.mixer.music.load('includes/music/dracu-level1.wav')
-    
-    # Used to manage how fast the screen updates
-    clock = pygame.time.Clock()
-
-    notif= "GAME OVER"
-    score = 0
  
     # -------- Main Program Loop -----------
     while not over:
@@ -180,7 +183,7 @@ def main():
             click = pygame.mouse.get_pressed()
 
             # if mouse hovered the play button
-            if 248+287 > mouse[0] > 248 and 136+150 > mouse[1] > 136:
+            if 190+450 > mouse[0] > 190 and 136+150 > mouse[1] > 136:
                 btn_play = pAH
                 if click[0] == 1:
                     main()
@@ -197,15 +200,24 @@ def main():
         # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
         current_level.draw(screen)
         active_sprite_list.draw(screen)
-                
-        textSurface, textRect = display_text(constants.WHITE,"Score: "+str(score), 20,600,0,150,50,)
-        screen.blit(textSurface, textRect)
-
+        
+        # display logo
         screen.blit(logo,(5,545))
-
+        
+        # display menu buttons if not play
         if not play:
-            screen.blit(btn_play,(248, 136))
+            if play_again:
+                x = 190
+            else:
+                x = 248
+            screen.blit(btn_play,(x, 136))
             screen.blit(btn_menu,(580,550))
+        # display the score otherwise :)
+        else: 
+            screen.blit(btn,(580,5))
+            textSurface, textRect = display_text(constants.WHITE,"Score: "+str(score), 20,600,0,150,50,)
+            screen.blit(textSurface, textRect)
+            
         # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
 
         # scoring depends on time
@@ -217,6 +229,10 @@ def main():
         # Updating the screen...
         pygame.display.flip()
 
+    # closing the window after executing the while loop...
+    pygame.quit()
+
  
 if __name__ == "__main__":
+    
     main()
